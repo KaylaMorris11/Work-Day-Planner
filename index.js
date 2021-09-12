ea3f16b419bc577e3a5ae86513d99ef99f73096d
Kayla Morris <moreiskay14@gmail.com>
2021-09-11 17:13:14 -0700

    Started Javascript to attempt to save input to screen

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do YYYY"));
 
-$(".saveBtn").click(function(event){
-
+$(".saveBtn").click(function(){
+    var input = $("textarea").value;
+    localStorage.setItem("inputText", input);
 })
 
+JSON.parse(localStorage.getItem("inputText"));
